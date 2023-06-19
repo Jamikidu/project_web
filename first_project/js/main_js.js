c53164b6178ca0f9929ef615447da315ac224996
Jamikidu <kangjh1994@gmail.com>
2023-06-20 02:35:21 +0900

    수정2

--- first_project/js/main_js.js
+++ first_project/js/main_js.js
@@ -2,21 +2,23 @@ window.onload = function(){
 // alert("testing");
 
 /* 메인사진변경함수 */
-let picture = ["./images/sokcho1.jpg", "./images/sokcho2.jpg",
-                "./images/sokcho3.jpg"]
+let picture = ["./images/sokcho1.jpg", "./images/sokcho2.jpg", "./images/sokcho3.jpg",
+                "./images/gangneung1.png", "./images/gangneung2.jpg", "./images/gangneung3.jpg" ]
 let imgIdx = 0;
+console.log(Math.round((Math.random()*picture.length)));
+let first_pic = Math.round((Math.random()*picture.length));
 
 changePicture();
 
 function changePicture(){
     let img = document.getElementById("pic1")
-    document.getElementById("pic1").src = picture[imgIdx];
-    imgIdx++;
+    img.src = picture[first_pic];
+    first_pic++;
 
-    if(imgIdx == picture.length){
-        imgIdx = 0;
+    if(first_pic == picture.length){
+        first_pic = 0;
     }
 
-    setTimeout(changePicture, 5000); //콜백함수
+    setTimeout(changePicture, 4000); //콜백함수
 } //
 }
